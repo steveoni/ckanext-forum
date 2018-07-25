c5f14eac004592117d66968f2e955e256922ddd1
Pavle Jonoski <pavle.jonoski@keitaro.com>
2018-07-25 16:44:16 +0200

    CKAN version 2.8 support

--- ckanext/issues/logic/action/action.py
+++ ckanext/issues/logic/action/action.py
@@ -506,8 +506,15 @@ def organization_users_autocomplete(context, data_dict):
 
     users = []
     for user in query.all():
-        user_dict = dict(user.__dict__)
-        user_dict.pop('_labels', None)
+        if isinstance(user, tuple):
+            user_dict = {
+                'id': user[0],
+                'name': user[1],
+                'fullname': user[2],
+            }
+        else:
+            user_dict = dict(user.__dict__)
+            user_dict.pop('_labels', None)
         users.append(user_dict)
     return users
 
