9650cd3d2f134dd9533cab7f8102521e5b72d669
Hristijan Vilos <hristijan.vilos@keitaro.com>
2021-09-01 15:08:13 +0200

    test_api, test_auth, test_validator migration

--- ckanext/issues/tests/logic/test_api.py
+++ ckanext/issues/tests/logic/test_api.py
@@ -1,22 +1,32 @@
-from ckan.tests import helpers
+import pytest
+
 from ckan.tests import factories
 
 from ckanext.issues.tests import factories as issue_factories
+from ckanext.issues.tests.fixtures import issues_setup, user
 
 
-class TestIssueApi(helpers.FunctionalTestBase):
-    def setup(self):
-        user = factories.User()
-        dataset = factories.Dataset()
+@pytest.fixture
+def dataset():
+    return factories.Dataset()
 
-        issue = issue_factories.Issue(user=user,
-                                      user_id=user['id'],
-                                      dataset_id=dataset['id'])
-        issue_factories.IssueComment(
-            user_id=user['id'],
-            issue_number=issue['number'],
-            dataset_id=issue['dataset_id'],
-        )
+@pytest.fixture
+def issue(user, dataset):
+    return issue_factories.Issue(user=user,
+                                 user_id=user['id'],
+                                 dataset_id=dataset['id'])
 
-    def test_search_api(self):
-        self._test_app.get("/api/3/action/issue_search", extra_environ={})
+@pytest.fixture
+def comment(issue, user):
+    comment = issue_factories.IssueComment(
+                    user_id=user['id'],
+                    issue_number=issue['number'],
+                    dataset_id=issue['dataset_id'],)
+    return comment
+
+class TestIssueApi(object):
+
+    @pytest.mark.usefixtures("clean_db", "issues_setup")
+    def test_search_api(self, app, user, dataset, issue, comment):
+        response = app.get("/api/3/action/issue_search", extra_environ={})
+        assert 200 == response._status_code
--- ckanext/issues/tests/logic/test_validators.py
+++ ckanext/issues/tests/logic/test_validators.py
@@ -1,27 +1,32 @@
+import pytest
+
 from ckan import model
 from ckan.lib import search
-from ckan.tests import factories, helpers
+from ckan.tests import factories
 
 from ckanext.issues.tests import factories as issue_factories
 from ckanext.issues.logic import validators
-from ckanext.issues.tests.helpers import ClearOnTearDownMixin
+from ckanext.issues.tests.fixtures import issues_setup
 
-from nose.tools import assert_equals
+@pytest.fixture
+def dataset():
+    return factories.Dataset()
 
-class TestAsPackageId(ClearOnTearDownMixin):
-    def setup(self):
-        self.dataset = factories.Dataset()
 
-    def test_given_name_returns_id(self):
-        package_id = validators.as_package_id(self.dataset['name'],
+class TestAsPackageId(object):
+
+    @pytest.mark.usefixtures("clean_db", "issues_setup")
+    def test_given_name_returns_id(self, dataset):
+        package_id = validators.as_package_id(dataset['name'],
                                               context={
                                                   'model': model,
                                                   'session': model.Session})
-        assert_equals(self.dataset['id'], package_id)
+        assert dataset['id'] == package_id
 
-    def test_given_id_returns_id(self):
-        package_id = validators.as_package_id(self.dataset['id'],
+    @pytest.mark.usefixtures("clean_db", "issues_setup")
+    def test_given_id_returns_id(self, dataset):
+        package_id = validators.as_package_id(dataset['id'],
                                               context={
                                                   'model': model,
                                                   'session': model.Session})
-        assert_equals(self.dataset['id'], package_id)
+        assert dataset['id'] == package_id
